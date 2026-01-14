b1bff0a524ad066939654837a04337fdd5546c25
Kyle Benacka <kylebenacka@outlook.com>
2026-01-14 11:20:16 -0600

    Added rating to ChoreHistory

--- ChoreJamming.Domain/Models/ChoreHistory.cs
+++ ChoreJamming.Domain/Models/ChoreHistory.cs
@@ -6,4 +6,6 @@ public class ChoreHistory
     public string ChoreName { get; set; } = string.Empty;
     public string SongTitle { get; set; } = string.Empty;
     public DateTime Date { get; set; } = DateTime.Now;
+    
+    public int Rating { get; set; } = 1;
 }
