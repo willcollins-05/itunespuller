74a509c403728a9cc8c9652d837b39959f815153
Kyle Benacka <kylebenacka@outlook.com>
2026-01-14 11:02:58 -0600

    Color for song cards

--- ChoreJamming.Application/obj/Debug/net9.0/ChoreJamming.Application.AssemblyInfo.cs
+++ ChoreJamming.Application/obj/Debug/net9.0/ChoreJamming.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChoreJamming.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a95d50ec90ad90d9b46221f47c08388aecaaaf45")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChoreJamming.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChoreJamming.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
